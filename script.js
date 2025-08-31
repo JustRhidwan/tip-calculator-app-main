e991516f5cba8845a64abd58d4152cf75a71d9be
JustRhidwan <107209973+JustRhidwan@users.noreply.github.com>
2025-08-31 11:52:28 +0100

    step

---
+++ script.js
@@ -0,0 +1,33 @@
+const bill = parseFloat(document.querySelector('#bill').value.trim());
+const people = parseInt(document.querySelector("#people").value.trim());
+const tip5 = document.querySelector("#tip-5");
+const tip10 = document.querySelector("#tip-10");
+const tip15 = document.querySelector("#tip-15");
+const tip25 = document.querySelector("#tip-25");
+const tip50 = document.querySelector("#tip-50");
+
+let x = '';
+function tipCalculator() {
+   tip5.addEventListener('click', function() {
+       x = Number(bill) * 0.05;
+       console.log(x);
+   });
+   tip10.addEventListener('click', function() {
+       x = Number(bill) * 0.1;
+       console.log(x);
+   });
+   tip15.addEventListener('click', function() {
+       x = Number(bill) * 0.15;
+       console.log(x);
+   });
+   tip25.addEventListener('click', function() {
+       x = Number(bill) * 0.25;
+       console.log(x);
+   });
+   tip50.addEventListener('click', function() {
+       x = Number(bill) * 0.5;
+       console.log(x);
+   });
+}
+
+tipCalculator();
