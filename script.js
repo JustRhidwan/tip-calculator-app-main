8226bb325d3a2955411955042a2499295558aff2
JustRhidwan <107209973+JustRhidwan@users.noreply.github.com>
2025-09-03 15:06:53 +0100

    trial

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const bill = parseFloat(document.querySelector('#bill').value.trim());
-const people = parseInt(document.querySelector("#people").value.trim());
-const tip5 = document.querySelector("#tip-5");
-const tip10 = document.querySelector("#tip-10");
-const tip15 = document.querySelector("#tip-15");
-const tip25 = document.querySelector("#tip-25");
-const tip50 = document.querySelector("#tip-50");
-const tipAmt = document.getElementById("tip-amount");
-const total = document.getElementById("total");
-
 function tipCalculator() {
-    let x = 0;
+     const bill = parseFloat(document.querySelector('#bill').value.trim());
+    const people = parseInt(document.querySelector("#people").value.trim());
+    let x = "";
+    const tip5 = document.querySelector("#tip-5");
+    const tip10 = document.querySelector("#tip-10");
+    const tip15 = document.querySelector("#tip-15");
+    const tip25 = document.querySelector("#tip-25");
+    const tip50 = document.querySelector("#tip-50");
+    const tipAmt = document.getElementById("tip-amount");
+    const total = document.getElementById("total");
+
    tip5.addEventListener('click', function() {
-       x = Number(bill) * 0.05;
-       tipAmt.textContent = x;
+       x = bill * 0.05;
+       console.log(x);
    });
    tip10.addEventListener('click', function() {
        x = Number(bill) * 0.1;
@@ -30,6 +30,6 @@ function tipCalculator() {
        x = Number(bill) * 0.5;
        console.log(x);
    });
+   tipAmt.textContent = x;
 }
-
-tipCalculator();
+ 
